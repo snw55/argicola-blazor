4e3dd768fc4d0507eeb42ebf2aa2a9200ec06dbd
snw55 <jordy.noom@franx.com>
2020-04-20 21:15:55 +0200

    Added storage extension. Changed injection of authentication service.

--- src/snw55-agricola-blazor-wasm/Program.cs
+++ src/snw55-agricola-blazor-wasm/Program.cs
@@ -1,8 +1,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Components.Authorization;
 using Blazor.Auth0;
+using Blazor.Extensions.Storage;
 
 namespace snw55_agricola_blazor_wasm
 {
@@ -22,7 +22,8 @@ namespace snw55_agricola_blazor_wasm
                     options.ClientId = "5DUd6KS3qMsRmRRvZpKQRm9fG2T6iARS";
                     options.RequireAuthenticatedUser = false;
                 })
-                .AddAuthorizationCore();
+                .AddAuthorizationCore()
+                .AddStorage();
 
             await builder.Build().RunAsync();
         }
